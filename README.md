9aefacd85d3bd2037fd5613bb10f5106991bc729
rody josue <rody.josue.cc.c@gmail.com>
2022-05-14 22:23:10 -0500

    add example of parameters for default

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Curso de Ecmascript
+
+### Parameters for default
+```
+function helloStudent(name='Josue',age=25,country='PE'){
+    this.name=name;
+    this.age=age;
+    this.country=country;
+    console.log(name,age,country)
+}
+helloStudent()
+
+helloStudent('Oscar',20,'MX')
+```
+
---
+++ app/es6/index.js
@@ -0,0 +1,17 @@
+function helloStudent(name='Josue',age=25,country='PE'){
+    this.name=name;
+    this.age=age;
+    this.country=country;
+
+    console.log(name,age,country)
+}
+
+helloStudent('Oscar',20,'MX');
+
+// spread operator
+const team1=['Maria','Carlos','Enrique'];
+const team2=['Josue','Cristhian','Sonia'];
+
+const education=['Mario',...team1,...team2];
+
+console.log(education);
